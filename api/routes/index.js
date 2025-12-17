15d65a91eba8aa6dddde36cd0b6a2ce8b7582737
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 13:24:33 +0100

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,11 +2,13 @@ import express from "express"
 import modulesRoutes from "./modules.js"
 import menuRoutes from "./menu.js"
 import actionsRoutes from "./actions.js"
+import kpiRoutes from "./kpi.js"
 
 const router = express.Router()
 
 router.use("/modules", modulesRoutes)
 router.use("/menu", menuRoutes)
 router.use("/actions", actionsRoutes)
+router.use("/kpi", kpiRoutes)
 
 export default router
