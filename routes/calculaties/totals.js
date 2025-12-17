f1fb31496428d7ba1b00156add8701ba3f302bfc
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 13:17:49 +0100

    Create totals.js

---
+++ routes/calculaties/totals.js
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
+
+export default async function handler(req, res) {
+  const { versionId } = req.params
+
+  const { data } = await supabase
+    .from("calc_totals")
+    .select("*")
+    .eq("calc_version_id", versionId)
+    .single()
+
+  res.json(data)
+}
