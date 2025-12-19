6c517042181cae4a1ac4ea6681afbc2e3ea3ea9f
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-19 09:11:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ import cors from "cors"
 import morgan from "morgan"
 import { logEvent } from "./utils/log.js"
 
+import apiRouter from "./api/routes/index.js"
+
 dotenv.config()
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
@@ -15,6 +18,8 @@ app.get("/ping", (req, res) => {
   logEvent("Ping ontvangen")
   res.json({ status: "ok", timestamp: Date.now() })
 })
+
+app.use("/api", apiRouter)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
