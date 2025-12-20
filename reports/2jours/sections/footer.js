fca8cab0c4c120a0e432794ace4c10fc3de658be
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-20 12:34:45 +0100

    Create footer.js

---
+++ reports/2jours/sections/footer.js
@@ -0,0 +1,10 @@
+export default function footer(doc) {
+  doc.fontSize(8)
+    .fillColor("#6b7280")
+    .text(
+      "Indicatief document – geen rechten kunnen worden ontleend aan deze calculatie",
+      36,
+      820,
+      { align: "center" }
+    )
+}
