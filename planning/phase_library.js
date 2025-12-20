d56b128f349adb0e847c81171c272e7f2a4bb036
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-20 12:45:42 +0100

    Create phase_library.js

---
+++ planning/phase_library.js
@@ -0,0 +1,12 @@
+export const PHASE_LIBRARY = [
+  { key: "init", naam: "Projectinitie", standaard_duur: 10 },
+  { key: "design", naam: "Ontwerp & engineering", standaard_duur: 20 },
+  { key: "permit", naam: "Vergunningen", standaard_duur: 30 },
+  { key: "prep", naam: "Werkvoorbereiding", standaard_duur: 15 },
+  { key: "foundation", naam: "Fundering", standaard_duur: 20 },
+  { key: "structure", naam: "Ruwbouw", standaard_duur: 40 },
+  { key: "envelope", naam: "Gevel & dak", standaard_duur: 25 },
+  { key: "install", naam: "Installaties E & W", standaard_duur: 30 },
+  { key: "finish", naam: "Afbouw", standaard_duur: 35 },
+  { key: "oplevering", naam: "Oplevering", standaard_duur: 10 }
+]
