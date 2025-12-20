1a98882016c55098abc31fa5ea8741accaf983e4
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-21 00:45:11 +0100

    Update generateCalculatiePdf.js

--- pdf/generateCalculatiePdf.js
+++ pdf/generateCalculatiePdf.js
@@ -1,1 +1,23 @@
 import PDFDocument from "pdfkit"
+import fs from "fs"
+
+export async function generateCalculatiePdf(data, outputPath) {
+  const doc = new PDFDocument({ size: "A4", margin: 40 })
+  doc.pipe(fs.createWriteStream(outputPath))
+  doc.font("Helvetica").fontSize(8)
+
+  let y = 80
+  data.forEach(r => {
+    doc.text(r.code, 40, y)
+    doc.text(r.omschrijving, 80, y)
+    doc.text(r.hoeveelheid, 260, y, { align: "right" })
+    doc.text(r.normuren, 300, y, { align: "right" })
+    doc.text(r.loonkosten, 360, y, { align: "right" })
+    doc.text(r.materiaalprijs, 420, y, { align: "right" })
+    doc.text(r.regel_totaal, 480, y, { align: "right" })
+    y += 12
+    if (y > 760) { doc.addPage(); y = 80 }
+  })
+
+  doc.end()
+}
